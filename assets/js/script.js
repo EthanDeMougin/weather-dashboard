50043dee5a3d2ee9a2357a1480386c09c4801b63
Ethan DeMougin <ethandemougin@gmail.com>
2022-08-18 20:04:18 -0400

    css adjustments and some history updates

---
+++ assets/js/script.js
@@ -0,0 +1,95 @@
+// global variables
+var apiKey = "1b18ce13c84e21faafb19c931bb29331";
+var savedSearches = [];
+
+// make list of previously searched cities
+var searchHistoryList = function(cityName) {
+    // create entry with city name
+    var searchHistoryEntry = $("<p>");
+    searchHistoryEntry.addClass("past-search");
+    searchHistoryEntry.text(cityName);
+
+    // create container for entry
+    var searchEntryContainer = $("<div>");
+    searchEntryContainer.addClass("past-search-container");
+
+    // append entry to container
+    searchEntryContainer.append(searchHistoryEntry);
+
+    // append entry container to search history container
+    var searchHistoryContainerEl = $("#search-history-container");
+    searchHistoryContainerEl.append(searchEntryContainer);
+
+    if (savedSearches.length > 0){
+        // update savedSearches array with previously saved searches
+        var previousSavedSearches = localStorage.getItem("savedSearches");
+        console.log(previousSavedSearches);
+        savedSearches = JSON.parse(previousSavedSearches);
+        console.log(savedSearches);
+    }
+
+    // add city name to array of saved searches
+    savedSearches.push(cityName);
+    localStorage.setItem("savedSearches", JSON.stringify(savedSearches));
+
+    // reset search input
+    $("#search-input").val("");
+};
+
+// load saved search history entries into search history container
+var loadSearchHistory = function() {
+    // get saved search history
+    var savedSearchHistory = localStorage.getItem("savedSearches");
+
+    // return false if there is no previous saved searches
+    if (!savedSearchHistory) {
+        return false;
+    }
+
+    // turn saved search history string into array
+    savedSearchHistory = JSON.parse(savedSearchHistory);
+
+    for (var i = 0; i < savedSearchHistory.length; i++) {
+        searchHistoryList(savedSearchHistory[i]);
+    }
+};
+
+var currentWeatherSection = function(cityName) {
+    console.log(cityName);
+
+    // get and use data from open weather current weather api end point
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`)
+        // get response and turn it into objects
+        .then(function(response) {
+            return response.json();
+        })
+        // get city's longitude and latitude
+        .then(function(response) {
+            var cityLon = response.coord.lon;
+            var cityLat = response.coord.lat;
+            console.log(cityLon);
+            console.log(cityLat);
+
+            // fetch(`http://`)
+        });
+};
+
+$("#search-form").on("submit", function() {
+    event.preventDefault();
+
+    // get name of city searched
+    var cityName = $("#search-input").val();
+
+    if (cityName === "" || cityName == null) {
+        //send alert if search input is empty when submitted
+        alert("Please enter name of city.");
+        event.preventDefault();
+    } else {
+        // if cityName is valid, add it to search history list and display its weather conditions
+        searchHistoryList(cityName);
+        currentWeatherSection(cityName);
+    }
+});
+
+loadSearchHistory();
+ 
